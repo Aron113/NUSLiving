dcb882db89ada3fa83a10ea5038dc75ac9d603d1
Zheng Yu <zhengyu@Zhengs-MacBook-Air.local>
2023-06-07 01:07:10 +0800

    done up some screens wgt tasks, currently wit dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:NUSLiving/models/task.dart';
 import 'package:flutter/material.dart';
-// import 'package:NUSLiving/screens/loginScreens/signup.dart';
+import 'package:NUSLiving/screens/loginScreens/signup.dart';
 import 'package:NUSLiving/screens/loginScreens/login.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:NUSLiving/screens/home.dart';
 //Firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:NUSLiving/screens/tasks_screens/tasks.dart';
+import 'package:NUSLiving/screens/tasks_screens/task_details_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +27,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NUSLiving',
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(225, 251, 252, 250),
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 213, 47, 47)),
+          seedColor: const Color.fromARGB(255, 213, 47, 47),
+          onPrimary: const Color.fromARGB(255, 00, 130, 128),
+          onSecondary: const Color.fromARGB(255, 54, 117, 136),
+          background: Colors.white,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
         useMaterial3: true,
       ),
-      home: Login(),
+      // home: Login(),
+      // home: Home(),
+      home: TaskDetailsPage(),
+      // home: const TasksPage(),
     );
   }
 }
