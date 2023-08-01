00dba7f14d240658e96084156a4d36c89bbe801f
Zheng Yu <zhengyu@Zhengs-MacBook-Air.local>
2023-08-01 09:54:14 +0800

    fixed model requirements for task brief description

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -23,7 +23,7 @@ const taskSchema = mongoose.Schema(
             type : String, 
             required : [true, "task must have content"],
             trim : true, 
-            minLength : [20, "task content must be at least 20 characters"],
+            minLength : [4, "task content must be at least 20 characters"],
             maxLength : [120, "task content must be at most 120 characters"],
             unique : false,
         },
